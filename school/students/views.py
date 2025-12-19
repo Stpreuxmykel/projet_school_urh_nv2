ce35ec0dd269bebc76320374d6ac7441780858dc
Mykel <stpreuxmykel@gmail.com>
2025-12-19 18:05:52 -0500

    first commit

---
+++ school/students/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
+from django.contrib import messages
+
+# Create your views here.
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students_list.html', {'students': students})
+
+
+def student_create(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Student has been added successfully !")
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'student_form.html',{'form':form})
+
+
+def student_update(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Student has been updated successfully !')
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'student_update.html', {'student': student})
+
+
+
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        student.delete()
+        messages.success(request, 'Student deleted successfully !')
+        return redirect('student_list')
+    return render(request, 'student_delete_confirm.html', {'student_information': student})
