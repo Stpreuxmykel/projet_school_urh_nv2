81fcb8b0532c3ba45524e71419f0a291a1795a50
Mykel <stpreuxmykel@gmail.com>
2026-01-09 18:06:35 -0500

    authentification

--- school/students/urls.py
+++ school/students/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path("", views.student_list, name="student_list"),
+    path("signup", views.student_signup, name="signup"),
+
+
+
     path("create/",views.student_create, name='student_create' ),
     path('update/<int:pk>/', views.student_update, name="student_update"),
     path('delete/<int:pk>/', views.student_delete, name="student_delete")
--- school/students/views.py
+++ school/students/views.py
@@ -2,12 +2,69 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from .forms import StudentForm
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def student_list(request):
     students = Student.objects.all()
     return render(request, 'students_list.html', {'students': students})
+
+
+def student_signup(request):
+    if request.method == "POST":
+
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+#       verification
+        if password != password2:
+            messages.error(request, 'Les mot de passe ne correspondent pas ')
+            return redirect('signup')
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Ce nom d'utilisateur existe deja ")
+            return redirect('signup')
+    
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Cet email est deja utilise ")
+            return redirect('signup')
+        
+#  end verification 
+
+        user = User.objects.create_user(
+            username=username,
+            email = email,
+            password=password2
+        )
+
+        user.save()
+        messages.success(request, 'Compte cree avec succes')
+        return redirect('student_list')
+
+
+    return render(request, 'signup.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def student_create(request):
